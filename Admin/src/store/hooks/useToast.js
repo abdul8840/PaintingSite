ae90f52bce6e66f560a43de1caf4a2cd08be4314
abdul8840 <abdul14941naseer@gmail.com>
2026-03-26 14:27:52 +0530

    created hooks for admin

---
+++ Admin/src/store/hooks/useToast.js
@@ -0,0 +1,12 @@
+import { useDispatch } from 'react-redux';
+import { showToast } from '../store/slices/uiSlice';
+
+export const useToast = () => {
+  const dispatch = useDispatch();
+  return {
+    success: (message) => dispatch(showToast({ message, type: 'success' })),
+    error: (message) => dispatch(showToast({ message, type: 'error' })),
+    info: (message) => dispatch(showToast({ message, type: 'info' })),
+    warning: (message) => dispatch(showToast({ message, type: 'warning' })),
+  };
+};
